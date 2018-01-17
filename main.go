754b97f22ef04822f4adbf2fc6936f8792e7b3d5
Nathan Alan Gilbert <nathan.gilbert@gmail.com>
2018-01-17 14:34:43 -0700

    basic program flow and duration

--- main.go
+++ main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strings"
+	"time"
+)
 
 func main() {
-	fmt.Println("hello world")
+	/*
+		arguments:
+		-mode1 X
+		-mode2 X [Y]
+	*/
+	options := os.Args[1:]
+	fmt.Println(options)
+
+	if strings.Compare(options[0], "-mode1") == 0 {
+		start := time.Now()
+		//TODO implement mode1
+		elapsed := time.Since(start)
+		ms := int64(elapsed / time.Millisecond)
+		s := float64(elapsed / time.Nanosecond)
+		fmt.Printf("Completed in %d milliseconds (%2.3f seconds)", ms, s)
+	} else if strings.Compare(options[0], "-mode2") == 0 {
+		start := time.Now()
+		//TODO implement mode2
+		elapsed := time.Since(start)
+		ms := int64(elapsed / time.Millisecond)
+		s := float64(elapsed / time.Nanosecond)
+		fmt.Printf("Completed in %d milliseconds (%2.3f seconds)", ms, s)
+	} else {
+		fmt.Println("Unknown option. Must be -mode1 or -mode2")
+		os.Exit(1)
+	}
 }
